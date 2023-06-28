1c14d44c01a761a66dbababd2834a25b8cdacf33
sook <sukkyu.chung@gmail.com>
2023-06-28 21:39:39 +0900

    add postcss & autoprefixer

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // to process `url(/next.svg)` in `globals.css`
+  // see: https://storybook.js.org/docs/react/configure/images-and-assets
+  staticDirs: ["../public"],
   addons: [],
   framework: {
     name: "@storybook/nextjs",
